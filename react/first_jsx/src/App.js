2fba4c7d94ab4c84caf8f51b125a1532288cb924
Dylan Martin <69947551+nalydmartin@users.noreply.github.com>
2021-01-05 17:09:07 -0800

    Added new files.

---
+++ react/first_jsx/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+
+function App() {
+  return (
+    <>
+    <h1>Hello Dojo!</h1>
+    <h3>Things I need to do:</h3>
+    <ul>
+      <li>Learn React</li>
+      <li>Climb Mt. Everest</li>
+      <li>Go to Austin</li>
+      <li>Rob a bank</li>
+    </ul>
+    </>
+  );
+}
+
+
+
+
+export default App;
